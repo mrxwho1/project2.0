179493e81db2be61fe3109b76484b9bb5573e87a
mrxwho1 <155052095+mrxwho1@users.noreply.github.com>
2024-02-24 22:27:33 +0300

    Add files via upload

---
+++ run7.py
@@ -0,0 +1,197 @@
+import random
+import pygame
+
+# Инициализация игры(модуль PyGame)
+pygame.init()
+
+clock = pygame.time.Clock()
+fps = 60
+
+screen_width = 864
+screen_height = 936
+
+# определение шрифта
+font = pygame.font.SysFont('Bauhaus 93', 60)
+
+# определите цвета
+white = (255, 255, 255)
+
+# Игровые переменные
+ground_scroll = 0
+scroll_speed = 4
+flying = False
+game_over = False
+pipe_gap = 150
+pipe_frequency = 1500  # значение в миллисекундах
+last_pipe = pygame.time.get_ticks() - pipe_frequency
+score = 0
+pass_pipe = False
+
+# Фон отображения игры
+# Импорт файлов из каталога
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Flappy Bird')
+bg = pygame.image.load('img/fon1.png')
+ground_img = pygame.image.load('img/ground.png')
+button_img = pygame.image.load('img/restart.png')
+
+def draw_text(text, font, text_color, x, y):
+    img = font.render(text, True, text_color)
+    screen.blit(img, (x, y))
+
+
+def reset_game():
+    pipe_group.empty()
+    flappy.rect.x = 100
+    flappy.rect.y = int(screen_height / 2)
+    score = 0
+    return score
+
+
+# Создание спрайтов
+class Bird(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.images = []
+        self.index = 0
+        self.counter = 0
+        for num in range(1, 4):
+            img = pygame.image.load(f'img/bird{num}.png')
+            self.images.append(img)
+        self.image = self.images[self.index]
+        self.rect = self.image.get_rect()
+        self.rect.center = [x, y]
+        self.vel = 0
+        self.clicked = False
+
+    def update(self):
+
+        # Гравитация игры
+
+        if flying is True:
+            self.vel += 0.5
+            if self.vel > 8:
+                self.vel = 8
+            if self.rect.bottom < 768:
+                self.rect.y += int(self.vel)
+
+        if game_over is False:
+            # Анимация прыжка
+
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+                self.clicked = True
+                self.vel = -10
+
+            if pygame.mouse.get_pressed()[0] == 0:
+                self.clicked = False
+
+            # Обработка анимации
+            self.counter += 1
+            flap_cooldown = 5
+
+            if self.counter > flap_cooldown:
+                self.counter = 0
+                self.index += 1
+                if self.index >= len(self.images):
+                    self.index = 0
+            self.image = self.images[self.index]
+
+            self.image = pygame.transform.rotate(self.images[self.index], int(self.vel * -2))
+        else:
+            self.image = pygame.transform.rotate(self.images[self.index], -90)
+
+
+class Pipe(pygame.sprite.Sprite):
+    def __init__(self, x, y, position):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('img/pipe.png')
+        self.rect = self.image.get_rect()
+        # позиция 1 - сверху, -1 - снизу
+        if position == 1:
+            self.image = pygame.transform.flip(self.image, False, True)
+            self.rect.bottomleft = [x, y - int(pipe_gap / 2)]
+        if position == -1:
+            self.rect.topleft = [x, y + int(pipe_gap / 2)]
+
+    def update(self):
+        self.rect.x -= scroll_speed
+        if self.rect.right < 0:
+            self.kill()
+
+bird_group = pygame.sprite.Group()
+pipe_group = pygame.sprite.Group()
+flappy = Bird(100, int(screen_height / 2))
+
+bird_group.add(flappy)
+
+button = Button(screen_width // 2 - 50, screen_height // 2 - 100, button_img)
+
+# - Основной игровой цикл -
+
+# Запуск игры
+run = True
+while run:
+
+    clock.tick(fps)
+
+    screen.blit(bg, (0, 0))
+
+    bird_group.draw(screen)
+    bird_group.update()
+
+    pipe_group.draw(screen)
+
+    # ground_img
+    screen.blit(ground_img, (ground_scroll, 768))
+
+    # Проверка результата
+    if len(pipe_group) > 0:
+        if bird_group.sprites()[0].rect.left > pipe_group.sprites()[0].rect.left \
+                and bird_group.sprites()[0].rect.right < pipe_group.sprites()[0].rect.right \
+                and pass_pipe == False:
+            pass_pipe = True
+        if pass_pipe == True:
+            if bird_group.sprites()[0].rect.left > pipe_group.sprites()[0].rect.right:
+                score += 1
+                pass_pipe = False
+
+    draw_text(str(score), font, white, int(screen_width / 2), 20)
+
+    # проверка колиции(столкновения)
+    if pygame.sprite.groupcollide(bird_group, pipe_group, False, False) or flappy.rect.top < 0:
+        game_over = True
+
+    # проверка, что птица не ударилась о землю
+    if flappy.rect.bottom >= 768:
+        game_over = True
+        flying = False
+
+    if game_over == False and flying == True:
+        # Генерирация труб(стволов)
+        time_now = pygame.time.get_ticks()
+        if time_now - last_pipe > pipe_frequency:
+            pipe_height = random.randint(-100, 100)
+            btm_pipe = Pipe(screen_width, int(screen_height / 2) + pipe_height, -1)
+            top_pipe = Pipe(screen_width, int(screen_height / 2) + pipe_height, 1)
+            pipe_group.add(btm_pipe, top_pipe)
+            last_pipe = time_now
+
+        ground_scroll -= scroll_speed
+        if abs(ground_scroll) > 35:
+            ground_scroll = 0
+        pipe_group.update()
+
+    # Проверка результата и рестрат игры
+   # !!!
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        if event.type == pygame.MOUSEBUTTONDOWN and flying == False and game_over == False:
+            flying = True
+
+    print(score)
+
+    pygame.display.update()
+
+pygame.quit()
